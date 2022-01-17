68a753e549ad9a106a98d2208aabe1ba29561996
hulluahven <hulluahven@gmail.com>
2022-01-17 22:49:47 +0300

    Исправлены пути для картинок, исправлено поведение меню при отключённом JS, согласно критерию Б4

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,23 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
-const pageHead = document.querySelector('.page-header');
+const pageHeader = document.querySelector('.page-header');
 const pageForm = document.querySelector('.contest-form');
 const surnameField = document.querySelector('#member-surname');
 const nameField = document.querySelector('#member-name');
 const mailField = document.querySelector('#member-mail');
-const failureMessage = document.querySelector('modal-failure');
-const successMessage = document.querySelector('modal-success');
+const failureMessage = document.querySelector('.modal-failure');
+const successMessage = document.querySelector('.modal-success');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
+
+if (pageHeader.classList.contains('page-header--closed')) {
+  pageHeader.classList.remove('page-header--closed');
+}
+else {
+  pageHeader.classList.add('page-header--closed');
+}
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
@@ -21,18 +30,20 @@ navToggle.addEventListener('click', function() {
 });
 
 navToggle.addEventListener('click', () => {
-  if(pageHead.classList.contains('page-header--closed')) {
-    pageHead.classList.remove('page-header--closed');
+  if(pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
   } else {
-    pageHead.classList.add('page-header--closed');
+    pageHeader.classList.add('page-header--closed');
   }
 });
 
 
-// pageForm.addEventListener('submit', function (event) {
-//   if(!surnameField.validity.valid) {
-//     // surnameField.style = 'outline: 5px solid green';
-//     failureMessage.classList.add('modal-failure--show');
-//     event.preventDefault();
-//   }
-// });
+  // pageForm.addEventListener('submit', (evt) => {
+  //   evt.preventDefault();
+  //   console.log('Отправлено')
+
+  //   if (!surnameField.value) {
+  //     successMessage.classList.add('modal-success--show');
+  //   }
+  // });
+  //------- не работает модалка с ошибкой, доделать--
